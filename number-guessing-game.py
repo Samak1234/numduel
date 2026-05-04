e04b5fee8b52d46840d8d7be77101cbcdc3e7da0
SAMAKSH DUBEY <samakshdubey9@gmail.com>
2026-05-04 16:59:55 +0530

    feat: add infinite game loop with break on correct guess

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -3,14 +3,14 @@ import random # Built-in module used to generate random numbers
 # Generate a random number between 1 and 100 (this is the secret answer)
 secret = random.randint(1,100)
 
-# Ask the user to guess the number and convert input to integer
-guess = int(input("Guessa number between 1 and 100: "))
-
-
+while True:
+   # Ask the user to guess the number and convert input to integer
+    guess = int(input("Guess a number between 1 and 100: "))
 # Compare user's guess with the secret number
-if guess == secret:
-    print("Congratulations,Correct") # User guessed correctly
-elif guess < secret:
-    print("Guess Higher number") # Hint: number is bigger
-else:
-    print("Guess lower number")  # Hint: number is smaller
+    if guess == secret:
+     print("Congratulations,Correct") # User guessed correctly
+     break # exits the loop
+    elif guess < secret:
+     print("Guess Higher number") # Hint: number is bigger
+    else:
+     print("Guess lower number")  # Hint: number is smaller
