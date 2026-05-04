2d5e256a0b9bd6bde02865af625e0f3f605f3115
SAMAKSH DUBEY <samakshdubey9@gmail.com>
2026-05-04 16:50:27 +0530

    docs: add explanatory comments to number guessing game

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,11 +1,16 @@
-import random #
+import random # Built-in module used to generate random numbers
 
+# Generate a random number between 1 and 100 (this is the secret answer)
 secret = random.randint(1,100)
+
+# Ask the user to guess the number and convert input to integer
 guess = int(input("Guessa number between 1 and 100: "))
 
+
+# Compare user's guess with the secret number
 if guess == secret:
-    print("Congratulations,Correct")
+    print("Congratulations,Correct") # User guessed correctly
 elif guess < secret:
-    print("Guess Higher number")
+    print("Guess Higher number") # Hint: number is bigger
 else:
-    print("Guess lower number")
+    print("Guess lower number")  # Hint: number is smaller
